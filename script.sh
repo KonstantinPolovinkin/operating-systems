0510e4b44715ed6f6cf63663cebf9f5a07106e48
unknown <kostyaman555@gmail.com>
2020-12-07 17:13:51 +0200

    initial commit

---
+++ script.sh
@@ -0,0 +1,7 @@
+echo "Now the time and date is --> $(date +"%Y-%m-%d %T")" > file.txt
+echo "The date now is --> $(date  +%Y-%m-%d)" >> file.txt
+echo "The time now is --> $(date  +%H:%M:%S)" >> file.txt
+
+cat file.txt
+xdg-open file.txt
+nano file.txt
