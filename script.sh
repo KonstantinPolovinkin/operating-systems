cd26353114e18e08d4da94fc382c9f7e1387aba3
unknown <kostyaman555@gmail.com>
2020-12-08 11:37:55 +0200

    fixed script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "Now the time and date is --> $(date +"%Y-%m-%d %T")" > file.txt
 echo "The date now is --> $(date  +%Y-%m-%d)" >> file.txt
 echo "The time now is --> $(date  +%H:%M:%S)" >> file.txt
